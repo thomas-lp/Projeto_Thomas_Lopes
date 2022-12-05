cf0eb8a220aa1c16e0d9ed5a73cb8166872a8e73
Thomas Anderson Lopes <thomas.lopes@estudante.ufla.br>
2022-12-04 22:28:33 -0300

    1

---
+++ src/B.java
@@ -0,0 +1,26 @@
+public class B {
+
+    private int B1;
+    private float B2;
+
+    public B() {
+        B1 = 0;
+        B2 = 0;
+    }
+
+    public int getB1() {
+        return B1;
+    }
+
+    public float getB2(){
+        return B2;
+    }
+
+    public void MB1() {
+        System.out.println("MB1");
+    }
+    
+    public void MB2() {
+        System.out.println("MB2");
+    }
+}
