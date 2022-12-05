ab09c24ee9bbdfe5f876f63f43d5578e046c6898
Thomas Anderson Lopes <thomas.lopes@estudante.ufla.br>
2022-12-05 00:08:02 -0300

    Criação classe D

---
+++ src/D.java
@@ -0,0 +1,18 @@
+public class D {
+    
+    private int D1;
+    private int D2;
+
+    public D() {
+        D1 = 0;
+        D2 = 0;
+    }
+
+    public void MD1() {
+        System.out.println("MD1");
+    }
+    
+    public void MD2() {
+        System.out.println("MD2");
+    }
+}
