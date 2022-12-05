87b34d48132eea89e63ff9770a91d75117f436f2
Thomas Anderson Lopes <thomas.lopes@estudante.ufla.br>
2022-12-04 23:00:35 -0300

    Criação da classe C

---
+++ src/C.java
@@ -0,0 +1,18 @@
+public class C {
+    
+    private String C1;
+    private int C2;
+
+    public C() {
+        C1 = null;
+        C2 = 0;
+    }
+
+    public void MC1() {
+        System.out.println("MC1");
+    }
+    
+    public void MC2() {
+        System.out.println("MC2");
+    }
+}
